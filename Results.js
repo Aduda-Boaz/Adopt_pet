d8f418eb7be92ec3978021d64cb784a918e5cc34
Aduda-Boaz <60380365+Aduda-Boaz@users.noreply.github.com>
2020-03-05 17:50:40 +0300

    Add files via upload

---
+++ Results.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Pet from './Pet';
+
+const Results = ({ pets }) => {
+    return (
+        <div className= "search">
+            {!pets.length ? (
+                <h1>No Pets Found</h1>
+            ) : (
+                pets.map((pet) => {
+                    return (
+                        <Pet
+                            id={pet.id}
+                            animal={pet.type}
+                            key={pet.id}
+                            breed={pet.breeds.primary}
+                            media={pet.photos}
+                            location={`${pet.contact.address.city}, ${pet.contact.address.state}`}
+                        />
+                    )
+                })
+            )}
+        </div>
+    )
+}
+export default Results;
---
+++ dropdown.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultState, options) => {
+  const [state, updateState] = useState(defaultState);
+  const id = `use-dropDown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => {
+    return (
+      <label htmlFor={id}>
+        {label}
+        <select
+          id={id}
+          value={state}
+          onChange={(e) => updateState(e.target.value)}
+          disabled={!options.length}
+        >
+          <option>All</option>
+          {options.map((option) => {
+            return (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+    );
+  };
+
+  return [state, Dropdown,updateState];
+};
+
+export default useDropdown; 
